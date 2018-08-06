3f38608cc183b612f5e63116cd25860082b02b0d
User <ppkrauss@gmail.com>
2018-08-05 22:43:05 -0300

    ultima versão sem JSONb no osmWd_raw-transform.sql

--- src/osmWd_raw-transform.sql
+++ src/osmWd_raw-transform.sql
@@ -35,7 +35,7 @@ CREATE TABLE wdosm.li_raw2 AS
            substr(ref,1,1) as ref_type,
            substr(ref,2)::bigint as ref_id
     FROM (
-      SELECT substr(osm_type,1,1) as osm_type,
+      SELECT substr(osm_type,1,1)::char as osm_type,
         osm_id::bigint as osm_id,
         regexp_split_to_table("otherIDs", ' ') as ref
       FROM wdosm.li_raw
@@ -51,13 +51,7 @@ CREATE UNIQUE INDEX wdosm_li_index_osmtyid ON wdosm.li_raw2 (osm_type,osm_id);
 ---------------
 --- LOCAL LIB:
 
-CREATE FUNCTION wdosm.n_wd_ids(  p_ids bigint[] ) RETURNS bigint[] AS $f$
-  SELECT array_agg(wd_ids[1])
-  FROM   wdosm.li_raw2
-  WHERE osm_type='n' AND osm_id IN (select unnest($1))
-$f$ language SQL IMMUTABLE;
-
-CREATE FUNCTION wdosm.w_wd_ids(  p_ids bigint[] , p_type text ) RETURNS bigint[] AS $f$
+CREATE FUNCTION wdosm.refs2wdids(  p_ids bigint[] , p_type char ) RETURNS bigint[] AS $f$
   SELECT CASE WHEN x='{}'::bigint[] THEN NULL ELSE x END
   FROM (
     SELECT array_agg_mult(wd_ids) as x
@@ -77,30 +71,26 @@ $f$ language SQL IMMUTABLE;
 -- passo 0 - revove nodes expurios
 DELETE FROM wdosm.li_raw2 WHERE osm_type='n' AND array_length(wd_ids,1)=0; -- nenhum
 
--- passo 1 - transcreve wd_ids dos nodes para as ways onde são membros, e esvazia n_refs
+-- passo 1 - transcreve wd_ids dos nodes para as ways onde são membros
 UPDATE wdosm.li_raw2
-  SET member_wd_ids = array_distinct_sort( wdosm.n_wd_ids(n_refs) )
+  SET member_wd_ids = array_distinct_sort( wdosm.refs2wdids(n_refs,'n') )
   WHERE osm_type IN ('w','r')
 ;
---UPDATE wdosm.li_raw2 SET n_refs=NULL;
-
--- passo 2 - transcreve wd_ids das ways para as relations onde são membros, e esvazia w_refs
+-- passo 2 - transcreve wd_ids das ways para as relations onde são membros
 UPDATE wdosm.li_raw2
-  SET member_wd_ids = array_merge_sort( member_wd_ids, wdosm.w_wd_ids(w_refs,'w') )
+  SET member_wd_ids = array_merge_sort( member_wd_ids, wdosm.refs2wdids(w_refs,'w') )
   -- não estamos pegando member_wd_ids das ways pois seria parentesco de segunda ordem.
   -- no futuro podemos mudar isso e fazer mais um merge.
   WHERE osm_type='r'
 ;
---UPDATE wdosm.li_raw2 SET w_refs=NULL;
-
+-- passo 3 - transcreve wd_ids das relations-filhas para as relations onde são membros
 UPDATE wdosm.li_raw2
-  SET member_wd_ids = array_merge_sort( member_wd_ids, wdosm.w_wd_ids(r_refs,'r') )
-  -- não estamos pegando member_wd_ids das ways pois seria parentesco de segunda ordem.
-  -- no futuro podemos mudar isso e fazer mais um merge.
+  SET member_wd_ids = array_merge_sort( member_wd_ids, wdosm.refs2wdids(r_refs,'r') )
   WHERE osm_type='r'
 ;
---UPDATE wdosm.li_raw2 SET r_refs=NULL;
 
+-- passo4 - (opcional) sumir com dados redundantes para não poluir.
+UPDATE wdosm.li_raw2 SET n_refs=NULL,w_refs=NULL,r_refs=NULL;
 DELETE FROM wdosm.li_raw2 WHERE member_wd_ids IS NULL AND wd_ids IS NULL;
 
 -----
