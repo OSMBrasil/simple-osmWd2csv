63707b85a46e49bbe85979a5fffd5152e3cefc58
User <ppkrauss@gmail.com>
2018-08-06 01:09:33 -0300

    JSONb and ops bug osmWd2csv.php corrected

--- src/osmWd2csv.php
+++ src/osmWd2csv.php
@@ -4,22 +4,12 @@
  */
 print "osm_type,osm_id,otherIDs";
 $r = new XMLReader; // any fast XML Pull parser (not DOM)
-if (!$r->open("php://stdin"))
-  die("\nFailed to open file\n");
+if (!$r->open("php://stdin"))  die("\nFailed to open file\n");
 $r->read();
-if ($r->name!='osm')
-  die("\n XML is not OSM\n");
-$tg=''; // taginfo
-while($r->read()) {
-  if ($r->nodeType == XMLReader::ELEMENT) {
-    $id = $r->getAttribute('id');
-    if ($r->name!='node' || $tg)
-      print "\n{$r->name},$id,$tg"; //".($tg? "\"$tg\"": '');
-    $tg = '';
-  } elseif ($r->nodeType == XMLReader::TEXT) {
-    $tg = trim( preg_replace('/\s+/s',' ',$r->value) );
-    //$tg = str_replace('"','""',$tg); // escape of CSV
-  }
-} // while
+if ($r->name!='osm')  die("\n XML is not OSM\n");
+while($r->read())
+  if ($r->nodeType == XMLReader::ELEMENT)
+    print "\n{$r->name},". $r->getAttribute('id') .',';
+  elseif ($r->nodeType == XMLReader::TEXT)
+    print trim( preg_replace('/\s+/s',' ',$r->value) );
 $r->close();
-
