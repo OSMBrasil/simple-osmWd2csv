782032353205ac70de249b6855e6d69c765f866d
Peter <ppkrauss@gmail.com>
2018-08-07 15:01:10 -0300

    issue #1, little optimization

--- src/osmWd2csv.php
+++ src/osmWd2csv.php
@@ -1,15 +1,30 @@
 <?php
 /**
- * OSM XML file with Wikidata tags, convert to CSV file.
+ * OSM XML file with Wikidata tags, convert to CSV file after pre-parse filter.
+ * The pre-parse pass only node/way/relation with optional wikidata and id-refs as content.
+ * @depends osmWd2csv_pre.sh
  */
 print "osm_type,osm_id,otherIDs";
-$r = new XMLReader; // any fast XML Pull parser (not DOM)
+define("ND",'node'); // 'node' or 'n'
+
+$r = new XMLReader; // any fast XML-Pull or SAX parser (not DOM!)
 if (!$r->open("php://stdin"))  die("\nFailed to open file\n");
 $r->read();
 if ($r->name!='osm')  die("\n XML is not OSM\n");
-while($r->read())
-  if ($r->nodeType == XMLReader::ELEMENT)
-    print "\n{$r->name},". $r->getAttribute('id') .',';
-  elseif ($r->nodeType == XMLReader::TEXT)
-    print trim( preg_replace('/\s+/s',' ',$r->value) );
+
+for( $lastLine=$tx='',$nm=ND;  $r->read();  )
+  if ($r->nodeType == XMLReader::ELEMENT) {
+    printLine($lastLine,$tx,$nm);
+    $tx = '';
+    $nm = $r->localName; //substr($r->localName,1); // results in n=node, w=way, r=relation
+    $lastLine = "$nm,". $r->getAttribute('id') .',';
+  } elseif ($r->nodeType == XMLReader::TEXT)
+    $tx = trim( preg_replace('/\s+/s',' ',$r->value) );
+
+printLine($lastLine,$tx,$nm);
 $r->close();
+
+function printLine($line,$tx,$nm) {
+  if ($nm!=ND || $tx) // exclude empty nodes
+    print "\n$line$tx";
+}
